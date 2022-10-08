57c6f6db1eef92400657be91d2840576725593e2
MaxSolider <abinglzb@gmail.com>
2022-10-08 12:13:05 +0800

    单向链表插入操作

--- structure/src/main/java/org/example/linkedlist/normal/NormalLinkedListOperate.java
+++ structure/src/main/java/org/example/linkedlist/normal/NormalLinkedListOperate.java
@@ -11,6 +11,7 @@ public class NormalLinkedListOperate {
 
     /**
      * 遍历链表，并返回链表长度
+     *
      * @param: headNode 链表头结点
      * @return: int 链表长度
      * @author: Max Solider
@@ -28,20 +29,113 @@ public class NormalLinkedListOperate {
     }
 
     /**
+     * 表头插入新结点
+     *
+     * @param: 链表头, 新结点
+     * @return: NormalListNode 新链表头
+     * @author: Max Solider
+     * @date: 2022/10/8 10:48
+     */
+    static NormalListNode insertHead(NormalListNode headNode, NormalListNode newNode) {
+        if (newNode == null) {
+            return headNode;
+        }
+        newNode.setNext(headNode);
+        headNode = newNode;
+        return headNode;
+    }
+
+    /**
+     * 表尾插入新结点
+     *
+     * @param: 链表头, 新结点
+     * @return: NormalListNode 链表头
+     * @author: Max Solider
+     * @date: 2022/10/8 10:48
+     */
+    static NormalListNode insertTail(NormalListNode headNode, NormalListNode newNode) {
+        // 如果是空链表，或新结点是null，则直接返回
+        if (headNode == null || newNode == null) {
+            return headNode;
+        }
+        NormalListNode current = headNode;
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(newNode);
+        return headNode;
+    }
+
+    /**
+     * 任意位置插入新结点
+     *
+     * @param headNode 链表表头
+     * @param newNode  新结点
+     * @param position 待插入位置
+     * @return 新链表表头
+     */
+    static NormalListNode insert(NormalListNode headNode, NormalListNode newNode, int position) {
+        int size = listLength(headNode);
+        // 插入位置合法性校验
+        if (position < 1 || position > size + 1) {
+            System.out.println("Position of node to insert is invalid. The valid inputs are 1 to " + (size + 1));
+            return headNode;
+        }
+        // 如果是在表头插入
+        if (position == 1) {
+            return insertHead(headNode, newNode);
+        }
+        // 表尾或中间位置插入，先找到待插入位置前一个结点
+        int currentPosition = 1;
+        NormalListNode previousNode = headNode;
+        while (currentPosition != position - 1) {
+            previousNode = previousNode.getNext();
+            currentPosition++;
+        }
+        // 将新结点的next指针指向待插入位置的原结点
+        newNode.setNext(previousNode.getNext());
+        // 将待插入位置前一个结点的next指针指向新结点
+        previousNode.setNext(newNode);
+        return headNode;
+    }
+
+
+    /**
      * 执行方法
+     *
      * @return: void
      * @author: Max Solider
      * @date: 2022/10/8 00:35
      */
     public static void main(String[] args) {
         NormalListNode headNode = initList();
+        // 1. 遍历链表
+//        listLength(headNode);
+        // 2. 表头插入新结点
+//        NormalListNode newNode = new NormalListNode(8);
+//        headNode = insertHead(headNode, newNode);
+        // 3. 表尾插入新结点
+//        NormalListNode newNode = new NormalListNode(8);
+//        headNode = insertTail(headNode, newNode);
+        // 4. 任意位置插入新结点
+        NormalListNode newNode = new NormalListNode(8);
+//        headNode = insert(headNode, newNode, 1);
+//        listLength(headNode);
+//        headNode = insert(headNode, newNode, 2);
+//        listLength(headNode);
+//        headNode = insert(headNode, newNode, 4);
+//        listLength(headNode);
+//        headNode = insert(headNode, newNode, 5);
+//        listLength(headNode);
+        headNode = insert(headNode, newNode, 6);
         listLength(headNode);
     }
 
     /**
      * 初始化链表
      * []
-     * @return: org.example.linkedlist.normal.NormalListNode 链表头
+     *
+     * @return: NormalListNode 链表头
      * @author: Max Solider
      * @date: 2022/10/8 00:36
      */
