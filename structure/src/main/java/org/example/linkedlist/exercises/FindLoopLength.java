f7963e80ada1e7c527b97b09b4c1de434acb25c6
MaxSolider <abinglzb@gmail.com>
2022-10-09 14:45:20 +0800

    fix

--- structure/src/main/java/org/example/linkedlist/exercises/FindLoopLength.java
+++ structure/src/main/java/org/example/linkedlist/exercises/FindLoopLength.java
@@ -3,42 +3,54 @@ package org.example.linkedlist.exercises;
 import org.example.linkedlist.normal.NormalListNode;
 
 /**
- * 判定链表中是否存在环
+ * 计算链表中环的长度
  *
- * @className: DoesLinkedListContainsLoop
+ * @className: FindLoopLength
  * @author: Max Solider
  * @date: 2022-10-09 10:43
  */
-public class DoesLinkedListContainsLoop {
+public class FindLoopLength {
 
     /**
-     * 判断链表中是否存在环
+     * 计算链表中环的长度
      *
-     * @param headNode
-     * @return boolean
      * @author: Max Solider
      * @date: 2022/10/9 14:18
+     * @param headNode
+     * @return int
      */
-    static boolean doesLinkedListContainsLoop(NormalListNode headNode) {
+    static int findLoopLength(NormalListNode headNode) {
         if (headNode == null || headNode.getNext() == null) {
-            return false;
+            return 0;
         }
         NormalListNode slowPtr = headNode, fastPtr = headNode;
+        boolean loopExists = false;
         while (fastPtr.getNext() != null && fastPtr.getNext().getNext() != null) {
             fastPtr = fastPtr.getNext().getNext();
             slowPtr = slowPtr.getNext();
             if (slowPtr == fastPtr) {
-                System.out.println("There is a loop in the linked list.");
-                return true;
+                System.out.println("There is a loop in the linked list. The meeting node's value is :" + slowPtr.getData());
+                loopExists = true;
+                break;
             }
         }
-        System.out.println("There is no loop in the linked list.");
-        return false;
+        if (!loopExists) {
+            System.out.println("There is no loop in the linked list.");
+            return 0;
+        }
+        int loopLength = 1;
+        slowPtr = slowPtr.getNext();
+        while (slowPtr != fastPtr) {
+            slowPtr = slowPtr.getNext();
+            loopLength++;
+        }
+        System.out.println("The loop in the linked list has " + loopLength + " nodes");
+        return loopLength;
     }
 
     public static void main(String[] args) {
         NormalListNode headNode = initList();
-        doesLinkedListContainsLoop(headNode);
+        findLoopLength(headNode);
     }
 
     /**
@@ -58,7 +70,7 @@ public class DoesLinkedListContainsLoop {
         node2.setNext(node3);
         node3.setNext(node4);
         node4.setNext(node5);
-//        node5.setNext(node3);
+        node5.setNext(node3);
         return node1;
     }
 }
